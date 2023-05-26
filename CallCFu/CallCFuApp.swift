c565ce2eefadfe0b2e196522ae74550cf63ee963
Ryu <85796869+Ryuuu825@users.noreply.github.com>
2023-05-26 16:50:33 +0800

    Initial Commit

---
+++ CallCFu/CallCFuApp.swift
@@ -0,0 +1,17 @@
+//
+//  CallCFuApp.swift
+//  CallCFu
+//
+//  Created by Ryu on 26/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct CallCFuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
