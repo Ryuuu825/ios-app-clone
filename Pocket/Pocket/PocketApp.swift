7b5b71f25217c298f2e0978a94cde206b44f40b8
Ryu <85796869+Ryuuu825@users.noreply.github.com>
2023-05-31 12:23:06 +0800

    add a new project

---
+++ Pocket/Pocket/PocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketApp.swift
+//  Pocket
+//
+//  Created by Ryu on 31/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct PocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
