a29dbb6fd6d4520e1bac4c5a07ee8d085c9bfaeb
Ryu <85796869+Ryuuu825@users.noreply.github.com>
2023-06-05 17:32:52 +0800

    add a new project ( clubhouse )

---
+++ Clubhouse/Clubhouse/ClubhouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClubhouseApp.swift
+//  Clubhouse
+//
+//  Created by Ryu on 5/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct ClubhouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
