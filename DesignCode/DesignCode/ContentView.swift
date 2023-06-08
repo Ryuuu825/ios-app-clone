dd032784ed061355abcace74b0604c595246913d
Ryu <85796869+Ryuuu825@users.noreply.github.com>
2023-06-08 10:14:49 +0800

    add a project

---
+++ DesignCode/DesignCode/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by Ryu on 8/6/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Hello, world!")
+            }
+            .navigationTitle("Explore")
+            .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            ContentView()
+        }
+    }
+}
---
+++ DesignCode/DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Ryu on 8/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
