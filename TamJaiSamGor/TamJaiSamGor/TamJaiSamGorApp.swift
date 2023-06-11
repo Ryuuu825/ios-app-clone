73e8eaae9242ef7182166bfcfc15898faa15da36
Ryu <85796869+Ryuuu825@users.noreply.github.com>
2023-06-11 23:13:50 +0800

    add tamjaisamgor

---
+++ TamJaiSamGor/TamJaiSamGor/TamJaiSamGorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamJaiSamGorApp.swift
+//  TamJaiSamGor
+//
+//  Created by Ryu on 11/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct TamJaiSamGorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
