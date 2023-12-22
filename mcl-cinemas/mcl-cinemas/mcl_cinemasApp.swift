126e52d4fd50a0885b837fe2b23c48bb0a02be0a
Ryu <85796869+Ryuuu825@users.noreply.github.com>
2023-12-22 11:18:29 +0800

    add mcl cinemas

---
+++ mcl-cinemas/mcl-cinemas/mcl_cinemasApp.swift
@@ -0,0 +1,17 @@
+//
+//  mcl_cinemasApp.swift
+//  mcl-cinemas
+//
+//  Created by Ryu on 22/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct mcl_cinemasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
