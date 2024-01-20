89fb522c1b4a9215636046800c844ddb6bbebeb4
Ryu <85796869+Ryuuu825@users.noreply.github.com>
2024-01-20 17:27:46 +0800

    add ig to projects

---
+++ Instagram/Instagram/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Instagram
+//
+//  Created by Ryu on 20/1/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Instagram/Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Ryu on 20/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
